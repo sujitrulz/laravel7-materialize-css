634c71cd36e131d7e58b80c073ff49eda068c131
Sujit Mourya <sujitm1987@gmail.com>
2020-06-01 21:04:53 +0530

    first commit

---
+++ src/MaterializeCSS.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Skydiver\LaravelMaterializeCSS;
+
+use Illuminate\Support\Facades\Facade;
+
+class MaterializeCSS extends Facade {
+
+    protected static function getFacadeAccessor() {
+        return 'materialize-css';
+    }
+
+}
+
+?>
